5277aad6494c3a997a28e5acdf7712995a1ec279
aldia_mechdream <futami16237@gmail.com>
2025-08-13 10:39:55 +0800

    feat: 用esm兼容cjs的写法重写了index.mjs； 测试基本完成，但是成员函数、async、生成器均无法new，是否为箭头函数还得找别的办法

--- tests/index.test.mjs
+++ tests/index.test.mjs
@@ -1,70 +1,77 @@
 import { it, describe } from 'node:test';
 import assert from 'node:assert';
 
-import { getFunctionKind } from '../dist/index.js';
+import { getFunctionKind } from '../lib/index.mjs';
 
 describe('getFunctionKind', () => {
   it('regular function', () => {
     function regularFunc() {}
     const flags = getFunctionKind(regularFunc);
 
-    assert(flags.isConstructor === true);
-    assert(flags.isAsyncFunction === false);
-    expect(flags.isGeneratorFunction === false);
+    assert.equal(flags.isConstructor, true);
+    assert.equal(flags.isAsyncFunction, false);
+    assert.equal(flags.isGeneratorFunction, false);
+    assert.equal(flags.isArrowFunction, false);
   });
 
   it('arrow function', () => {
     const arrowFunc = () => {};
     const flags = getFunctionKind(arrowFunc);
 
-    assert(flags.isConstructor === false);
-    assert(flags.isAsyncFunction === false);
-    assert(flags.isGeneratorFunction === false);
+    assert.equal(flags.isConstructor, false);
+    assert.equal(flags.isAsyncFunction, false);
+    assert.equal(flags.isGeneratorFunction, false);
+    assert.equal(flags.isArrowFunction, true);
   });
 
   it('async function', () => {
     async function asyncFunc() {}
     const flags = getFunctionKind(asyncFunc);
 
-    assert(flags.isConstructor === false);
-    assert(flags.isAsyncFunction === true);
-    assert(flags.isGeneratorFunction === false);
+    assert.equal(flags.isConstructor, false);
+    assert.equal(flags.isAsyncFunction, true);
+    assert.equal(flags.isGeneratorFunction, false);
+    assert.equal(flags.isArrowFunction, null);
   });
 
   it('async arrow function', () => {
     const asyncArrowFunc = async () => {};
     const flags = getFunctionKind(asyncArrowFunc);
 
-    assert(flags.isConstructor === false);
-    assert(flags.isAsyncFunction === true);
-    assert(flags.isGeneratorFunction === false);
+    assert.equal(flags.isConstructor, false);
+    assert.equal(flags.isAsyncFunction, true);
+    assert.equal(flags.isGeneratorFunction, false);
+    assert.equal(flags.isArrowFunction, null);
   });
 
   it('generator function', () => {
     function* generatorFunc() {}
     const flags = getFunctionKind(generatorFunc);
 
-    assert(flags.isConstructor === false);
-    assert(flags.isAsyncFunction === false);
-    assert(flags.isGeneratorFunction === true);
+    assert.equal(flags.isConstructor, false);
+    assert.equal(flags.isAsyncFunction, false);
+    assert.equal(flags.isGeneratorFunction, true);
+    assert.equal(flags.isArrowFunction, null);
   });
 
   it('async generator function', () => {
     async function* asyncGeneratorFunc() {}
     const flags = getFunctionKind(asyncGeneratorFunc);
 
-    assert(flags.isConstructor === false);
-    assert(flags.isAsyncFunction === true);
-    assert(flags.isGeneratorFunction === true);
+    assert.equal(flags.isConstructor, false);
+    assert.equal(flags.isAsyncFunction, true);
+    assert.equal(flags.isGeneratorFunction, true);
+    assert.equal(flags.isArrowFunction, null);
   });
 
   it('class constructor', () => {
     class TestClass {}
     const flags = getFunctionKind(TestClass);
 
-    assert(flags.isConstructor === true);
-    assert(flags.isAsyncFunction === false);
-    assert(flags.isGeneratorFunction === false);
+    assert.equal(flags.isConstructor, true);
+    assert.equal(flags.isAsyncFunction, false);
+    assert.equal(flags.isGeneratorFunction, false);
+    assert.equal(flags.isArrowFunction, false);
   });
 
   it('bound function', () => {
@@ -72,9 +79,10 @@ describe('getFunctionKind', () => {
     const boundFunc = originalFunc.bind(null);
     const flags = getFunctionKind(boundFunc);
 
-    assert(flags.isConstructor === true);
-    assert(flags.isAsyncFunction === false);
-    assert(flags.isGeneratorFunction === false);
+    assert.equal(flags.isConstructor, true);
+    assert.equal(flags.isAsyncFunction, false);
+    assert.equal(flags.isGeneratorFunction, false);
+    assert.equal(flags.isArrowFunction, false);
   });
 
   it('bound arrow function', () => {
@@ -82,9 +90,10 @@ describe('getFunctionKind', () => {
     const boundArrowFunc = originalArrowFunc.bind(null);
     const flags = getFunctionKind(boundArrowFunc);
 
-    assert(flags.isConstructor === false);
-    assert(flags.isAsyncFunction === false);
-    assert(flags.isGeneratorFunction === false);
+    assert.equal(flags.isConstructor, false);
+    assert.equal(flags.isAsyncFunction, false);
+    assert.equal(flags.isGeneratorFunction, false);
+    assert.equal(flags.isArrowFunction, true);
   });
 
   it('bound async function', () => {
@@ -92,9 +101,10 @@ describe('getFunctionKind', () => {
     const boundAsyncFunc = originalAsyncFunc.bind(null);
     const flags = getFunctionKind(boundAsyncFunc);
 
-    assert(flags.isConstructor === false);
-    assert(flags.isAsyncFunction === false);
-    assert(flags.isGeneratorFunction === false);
+    assert.equal(flags.isConstructor, false);
+    assert.equal(flags.isAsyncFunction, false);
+    assert.equal(flags.isGeneratorFunction, false);
+    assert.equal(flags.isArrowFunction, null);
   });
 
   it('bound generator function', () => {
@@ -102,9 +112,10 @@ describe('getFunctionKind', () => {
     const boundGeneratorFunc = originalGeneratorFunc.bind(null);
     const flags = getFunctionKind(boundGeneratorFunc);
 
-    assert(flags.isConstructor === false);
-    assert(flags.isAsyncFunction === false);
-    assert(flags.isGeneratorFunction === false);
+    assert.equal(flags.isConstructor, false);
+    assert.equal(flags.isAsyncFunction, false);
+    assert.equal(flags.isGeneratorFunction, false);
+    assert.equal(flags.isArrowFunction, null);
   });
 
   it('bound class constructor', () => {
@@ -112,17 +123,19 @@ describe('getFunctionKind', () => {
     const boundClass = TestClass.bind(null);
     const flags = getFunctionKind(boundClass);
 
-    assert(flags.isConstructor === true);
-    assert(flags.isAsyncFunction === false);
-    assert(flags.isGeneratorFunction === false);
+    assert.equal(flags.isConstructor, true);
+    assert.equal(flags.isAsyncFunction, false);
+    assert.equal(flags.isGeneratorFunction, false);
+    assert.equal(flags.isArrowFunction, false);
   });
 
   it('native function', () => {
     const flags = getFunctionKind(Array.isArray);
 
-    assert(flags.isConstructor === false);
-    assert(flags.isAsyncFunction === false);
-    assert(flags.isGeneratorFunction === false);
+    assert.equal(flags.isConstructor, false);
+    assert.equal(flags.isAsyncFunction, false);
+    assert.equal(flags.isGeneratorFunction, false);
+    assert.equal(flags.isArrowFunction, false);
   });
 
   it('method function', () => {
@@ -131,9 +144,11 @@ describe('getFunctionKind', () => {
     };
     const flags = getFunctionKind(obj.method);
 
-    assert(flags.isConstructor === false);
-    assert(flags.isAsyncFunction === false);
-    assert(flags.isGeneratorFunction === false);
+    assert.equal(flags.isConstructor, false);
+    assert.equal(flags.isAsyncFunction, false);
+    assert.equal(flags.isGeneratorFunction, false);
+    // fixme 成员函数也不能new！
+    assert.equal(flags.isArrowFunction, false);
   });
 
   it('async method function', () => {
@@ -142,9 +157,10 @@ describe('getFunctionKind', () => {
     };
     const flags = getFunctionKind(obj.method);
 
-    assert(flags.isConstructor === false);
-    assert(flags.isAsyncFunction === true);
-    assert(flags.isGeneratorFunction === false);
+    assert.equal(flags.isConstructor, false);
+    assert.equal(flags.isAsyncFunction, true);
+    assert.equal(flags.isGeneratorFunction, false);
+    assert.equal(flags.isArrowFunction, null);
   });
 
   it('generator method function', () => {
@@ -153,9 +169,10 @@ describe('getFunctionKind', () => {
     };
     const flags = getFunctionKind(obj.method);
 
-    assert(flags.isConstructor === true);
-    assert(flags.isAsyncFunction === false);
-    assert(flags.isGeneratorFunction === true);
+    assert.equal(flags.isConstructor, false);
+    assert.equal(flags.isAsyncFunction, false);
+    assert.equal(flags.isGeneratorFunction, true);
+    assert.equal(flags.isArrowFunction, null);
   });
 
   it('error handling - non-function input', () => {
